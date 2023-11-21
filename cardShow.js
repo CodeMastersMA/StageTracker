6848108c4436d04a4be4190a0eb48c26af7a5675
Solemadd <hell.maee@gmail.com>
2023-11-21 11:21:07 +0100

    J'ai dead ça. Fonction d'affichage faite.

---
+++ cardShow.js
@@ -0,0 +1,16 @@
+
+
+
+export function cardLoad(list){
+    console.log("loaded")
+    {list.forEach(element => {
+        const section = document.querySelector("#section")
+        const card = document.createElement("div")
+        card.innerHTML=`
+        <div id="cards"><div class="card" style="width: 18rem;">
+        <img id="img" src="${element.image}" class="card-img-top" alt="https://static.vecteezy.com/ti/vecteur-libre/p3/6122514-musique-concert-scene-fond-vectoriel.jpg">
+        <div class="card-body"><h2 id="title">${element.title_fr}</h2><p class="card-text" id="desc">${element.description_fr}</p></div></div></div>
+        `
+        section.appendChild(card)    
+    })
+}}
--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-const reponse = await fetch("https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/evenements-publics-openagenda/records?limit=20&refine=keywords_fr%3A%22concert%22&refine=location_countrycode%3A%22FR%22&refine=lastdate_begin%3A%222023%22");
-const ods = await reponse.json()
-
-/*boutton de test du json*/document.querySelector("#test").addEventListener("click", () => console.log(ods) )
-
 /* 
 CE SCRIPT SERT A CHARGER LES AUTRES SCRIP, LES API, ET A LANCER LES FONCTION 0 PARTIR DES BOUTTONS.
 
 /!!!\ PAS TOUCHER /!!!\
 */
+
+// LOAD API
+const reponse = await fetch("https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/evenements-publics-openagenda/records?limit=20&refine=keywords_fr%3A%22concert%22&refine=location_countrycode%3A%22FR%22&refine=lastdate_begin%3A%222023%22");
+const ods = await reponse.json()
+//IMPORT FUNCTIONS
+import * as fnct from "/cardShow.js"
+//INIT VARIABLES
+console.log(ods)
+const list = ods.results
+
+
+//LOAD DE LA PAGE DE BASE
+fnct.cardLoad(list)
+
+
+
+
+
+/*boutton de test du json*/document.querySelector("#test").addEventListener("click", () => console.log(ods) )
+
+
 
 
 
